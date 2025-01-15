ee55d3c67f06df9cdc67bd9c63c0fd49de27e3b8
ArengDev <97821562+Areng14@users.noreply.github.com>
2025-01-15 10:56:47 +0700

    started project page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,14 +1,60 @@
-import { Box } from "@mui/material";
-import React from "react";
+import { Box, Container, Grid2, Typography } from "@mui/material";
+import ImageSlider from "../components/ImageSlider";
 
-const ProjectPage = () => {
+export default function Projects() {
+
+  const images = [
+    "/misc/mainslide/img1.png",
+    "/misc/mainslide/img2.png",
+    "/misc/mainslide/img3.png",
+    "/misc/mainslide/img4.png",
+  ];
+
   return (
     <Box>
-        <p>
-            Still working on this page
-        </p>
+      <Box sx={{ minHeight: "100vh", display: "flex", alignItems: "center"}}>
+        <ImageSlider imgs={images}/>
+        <Container maxWidth={false} sx={{ maxWidth: "1600px", minWidth: {
+          xs: "500px",
+          md: "1600px",
+        }}}>
+          <Grid2
+            container
+            spacing={6}
+            sx={{
+              py: { xs: 4, md: 8 },
+              alignItems: "center",
+              px: 4,
+            }}
+          >
+            {/* Text Content Section */}
+            <Box>
+              <Typography
+                variant="h2"
+                sx={{
+                  fontSize: { xs: "2rem", sm: "2.5rem", md: "5rem" },
+                  mb: 5,
+                  wordBreak: "break-word",
+                  fontWeight: 700,
+                }}
+              >
+                Projects
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: { xs: "1rem", md: "1.1rem" },
+                  maxWidth: { xs: "100%", md: "50%" },
+                }}
+              >
+                {/* TODO: Write projects */}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua,
+                facilitating seamless solutions for diverse clients.              
+              </Typography>
+            </Box>
+          </Grid2>
+        </Container>
+      </Box>
     </Box>
   );
 }
-
-export default ProjectPage;
