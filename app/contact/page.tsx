cf2c1d63fa85762b3c734b5631b33ec45eb10992
ArengDev <kktdell.1@gmail.com>
2025-01-16 23:32:16 +0700

    start contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,78 @@
+// app/projects/page.tsx
+import { Metadata } from 'next';
+import { Box, Container, Grid2, Link, Typography } from "@mui/material";
+import ImageSlider from "../components/ImageSlider";
+
+const images: string[] = [
+  "/misc/mainslide/img5.png",
+  "/misc/mainslide/img6.png",
+  "/misc/mainslide/img7.png",
+  "/misc/mainslide/img8.png",
+];
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: 'About',
+    description: 'About me',
+  };
+};
+
+interface ProjectsPageProps {
+  searchParams: Promise<{ search?: string }>;
+}
+
+export default function About() {
+  return (
+    <Box>
+      <Box sx={{ minHeight: "100vh", display: "flex", alignItems: "center" }}>
+        <ImageSlider imgs={images} />
+        <Container 
+          maxWidth={false} 
+          sx={{ maxWidth: "1600px", minWidth: { xs: "500px", md: "1600px" } }}
+        >
+          <Grid2
+            container
+            spacing={6}
+            sx={{
+              py: { xs: 4, md: 8 },
+              alignItems: "center",
+              px: 4,
+            }}
+          >
+            {/* Text Content Section */}
+            <Box>
+              <Typography
+                variant="h2"
+                sx={{
+                  fontSize: { xs: "2rem", sm: "2.5rem", md: "5rem" },
+                  mb: 5,
+                  wordBreak: "break-word",
+                  fontWeight: 700,
+                }}
+              >
+                Contact
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: { xs: "1rem", md: "1.1rem" },
+                  maxWidth: { xs: "60%", md: "50%" },
+                }}
+              >
+                A list of ways you can contact me for whatever reason.
+                I am mainly active on Discord, and I check my email every now and then.
+                Choose whatever way is most convenient for you.
+              </Typography>
+            </Box>
+          </Grid2>
+        </Container>
+      </Box>
+
+      {/* Content */}
+      <Box sx={{ paddingTop: 16 }}>
+        <Container maxWidth={false} sx={{ maxWidth: "1600px", minWidth: { xs: "500px", md: "1600px" } }}>
+            
+        </Container>
+      </Box>
+    </Box>
+  );
+}
