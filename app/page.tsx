d6452bc5eb2e914dfa587616650594802633442c
ArengDev <97821562+Areng14@users.noreply.github.com>
2025-01-17 11:55:49 +0700

    Update page.tsx
    removed duplicate ts
    dont know how this keeps getting here

--- app/page.tsx
+++ app/page.tsx
@@ -92,15 +92,6 @@ export default function Home() {
               <Grid2 size={{ xs: 12, md: 6 , lg: 4}}>
                 <SkillCard
                 skill="TS"
-                colors={["rgb(3, 90, 148)", "rgb(35, 112, 154)", "rgb(62, 133, 177)"]}
-                angle="45deg"
-                icon={"/misc/skills/ts.svg"}
-                endpoint="/projects?search=TypeScript"/>              
-              </Grid2>
-              {/* Typescript */}
-              <Grid2 size={{ xs: 12, md: 6 , lg: 4}}>
-                <SkillCard
-                skill="TS"
                 colors={["rgb(0, 93, 155)", "rgb(35, 112, 154)", "rgb(62, 133, 177)"]}
                 angle="45deg"
                 icon={"/misc/skills/ts.svg"}
