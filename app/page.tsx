7de3b314a0b9d3210d09e0af947feb20e437399f
waritk <84290064+waritk@users.noreply.github.com>
2025-02-09 16:12:38 +0700

    update model from backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-"use client"
+"use client";
 import { Box, Container, Grid2, Typography } from "@mui/material";
 import ImageSlider from "./components/ImageSlider";
 import SkillCard from "./components/SkillCard";
 import getSkills from "./getSkills";
 import { SKILL_ENDPOINT } from "./api/api_service";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Skill } from "./types/skill";
 
 export default function Home() {
+  const [languagesSkill, setLanguagesSkill] = useState<Skill[]>([]);
 
   useEffect(() => {
     fetchSkills();
@@ -14,8 +16,12 @@ export default function Home() {
 
   const fetchSkills = async () => {
     const response = await fetch(SKILL_ENDPOINT.getAllSkills);
-    const data = await response.json();
-  }
+    const data: Skill[] = await response.json();
+
+    if (response.ok) {
+      setLanguagesSkill(data.filter((skill) => skill.skillType === "Language"));
+    }
+  };
 
   const images = [
     "/misc/mainslide/img1.png",
@@ -26,12 +32,18 @@ export default function Home() {
 
   return (
     <Box>
-      <Box sx={{ minHeight: "100vh", display: "flex", alignItems: "center"}}>
-        <ImageSlider imgs={images}/>
-        <Container maxWidth={false} sx={{ maxWidth: "1600px", minWidth: {
-          xs: "500px",
-          md: "1600px",
-        }}}>
+      <Box sx={{ minHeight: "100vh", display: "flex", alignItems: "center" }}>
+        <ImageSlider imgs={images} />
+        <Container
+          maxWidth={false}
+          sx={{
+            maxWidth: "1600px",
+            minWidth: {
+              xs: "500px",
+              md: "1600px",
+            },
+          }}
+        >
           <Grid2
             container
             spacing={6}
@@ -62,9 +74,9 @@ export default function Home() {
                   maxWidth: { xs: "60%", md: "50%" },
                 }}
               >
-                A software developer who makes programs on his freetime,
-                ranging from things that are useless to things that are kinda useful.
-                I try to keep my projects as clean and readable.
+                A software developer who makes programs on his freetime, ranging
+                from things that are useless to things that are kinda useful. I
+                try to keep my projects as clean and readable.
               </Typography>
             </Box>
           </Grid2>
@@ -72,18 +84,38 @@ export default function Home() {
       </Box>
 
       {/* Skills */}
-      <Box sx={{ paddingTop: 16}}>
-        <Container maxWidth={false} sx={{ maxWidth: "1600px"}}>       
-            <Typography variant="h1" sx={{fontWeight: 700, fontSize: {xs: '3.5rem',sm: '6rem'}}}>
-              Skills
-            </Typography>
+      <Box sx={{ paddingTop: 16 }}>
+        <Container maxWidth={false} sx={{ maxWidth: "1600px" }}>
+          <Typography
+            variant="h1"
+            sx={{ fontWeight: 700, fontSize: { xs: "3.5rem", sm: "6rem" } }}
+          >
+            Skills
+          </Typography>
 
-            {/* Languages */}
-            <Typography variant="h2" sx={{fontWeight: 500, paddingTop: 6, fontSize: {xs: '3rem',sm: '3.75rem'}}}>
-              Languages
-            </Typography>
-            <Grid2 container spacing={4} sx={{ paddingTop: 4 }}>
-              {getSkills()["Languages"].map((skill) => (
+          {/* Languages */}
+          <Typography
+            variant="h2"
+            sx={{
+              fontWeight: 500,
+              paddingTop: 6,
+              fontSize: { xs: "3rem", sm: "3.75rem" },
+            }}
+          >
+            Languages
+          </Typography>
+          <Grid2 container spacing={4} sx={{ paddingTop: 4 }}>
+            {languagesSkill.map((skill) => (
+              <Grid2 key={skill.id} size={{ xs: 12, md: 6, lg: 4 }}>
+                <SkillCard
+                  skill={skill.skillName}
+                  colors={skill.gradientColor}
+                  angle={`${skill.gradientAngle}deg`}
+                  icon={skill.icon}
+                />
+              </Grid2>
+            ))}
+            {/* {getSkills()["Languages"].map((skill) => (
                 <Grid2 key={crypto.randomUUID()} size={{ xs: 12, md: 6 , lg: 4}}>
                   <SkillCard
                     skill={skill["skill"]}
@@ -92,42 +124,56 @@ export default function Home() {
                     icon={skill["icon"]}
                     />   
                 </Grid2>   
-                ))}
-            </Grid2>
+                ))} */}
+          </Grid2>
 
-            {/* Frameworks */}
-            <Typography variant="h2" sx={{fontWeight: 500, paddingTop: 6, fontSize: {xs: '3rem',sm: '3.75rem'}}}>
-              Frameworks
-            </Typography>
-            <Grid2 container spacing={4} sx={{ paddingTop: 4 }}>
-              {getSkills()["Frameworks"].map((skill) => (
-                <Grid2 key={crypto.randomUUID()} size={{ xs: 12, md: 6 , lg: 4}}>
-                  <SkillCard
-                    skill={skill["skill"]}
-                    colors={skill["colors"]}
-                    angle={skill["angle"]}
-                    icon={skill["icon"]}     
-                    />   
-                </Grid2>   
-                ))}
-            </Grid2>
+          {/* Frameworks */}
+          <Typography
+            variant="h2"
+            sx={{
+              fontWeight: 500,
+              paddingTop: 6,
+              fontSize: { xs: "3rem", sm: "3.75rem" },
+            }}
+          >
+            Frameworks
+          </Typography>
+          <Grid2 container spacing={4} sx={{ paddingTop: 4 }}>
+            {getSkills()["Frameworks"].map((skill) => (
+              <Grid2 key={crypto.randomUUID()} size={{ xs: 12, md: 6, lg: 4 }}>
+                <SkillCard
+                  skill={skill["skill"]}
+                  colors={skill["colors"]}
+                  angle={skill["angle"]}
+                  icon={skill["icon"]}
+                />
+              </Grid2>
+            ))}
+          </Grid2>
 
-            {/* Design */}
-            <Typography variant="h2" sx={{fontWeight: 500, paddingTop: 6, fontSize: {xs: '3rem',sm: '3.75rem'}}}>
-              Design Tools
-            </Typography>
-            <Grid2 container spacing={4} sx={{ paddingTop: 4 }}>
-              {getSkills()["DesignTools"].map((skill) => (
-                <Grid2 key={crypto.randomUUID()} size={{ xs: 12, md: 6 , lg: 4}}>
-                  <SkillCard
-                    skill={skill["skill"]}
-                    colors={skill["colors"]}
-                    angle={skill["angle"]}
-                    icon={skill["icon"]}   
-                    />   
-                </Grid2>   
-                ))}
-            </Grid2>
+          {/* Design */}
+          <Typography
+            variant="h2"
+            sx={{
+              fontWeight: 500,
+              paddingTop: 6,
+              fontSize: { xs: "3rem", sm: "3.75rem" },
+            }}
+          >
+            Design Tools
+          </Typography>
+          <Grid2 container spacing={4} sx={{ paddingTop: 4 }}>
+            {getSkills()["DesignTools"].map((skill) => (
+              <Grid2 key={crypto.randomUUID()} size={{ xs: 12, md: 6, lg: 4 }}>
+                <SkillCard
+                  skill={skill["skill"]}
+                  colors={skill["colors"]}
+                  angle={skill["angle"]}
+                  icon={skill["icon"]}
+                />
+              </Grid2>
+            ))}
+          </Grid2>
         </Container>
       </Box>
     </Box>
---
+++ app/types/skill.ts
@@ -0,0 +1,8 @@
+export interface Skill {
+  id: string;
+  skillName: string;
+  gradientColor: string[];
+  gradientAngle: number;
+  icon: string;
+  skillType: string;
+}
